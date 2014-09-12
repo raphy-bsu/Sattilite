1894507c70266ecf1b4dd75ee65348f7407eab12
Orel Sokolov <orelcokolov@gmail.com>
2014-09-12 12:03:39 +0300

    Implement saving

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,15 @@
 class PublicController < ApplicationController
   def index
+    if File.exists? File.join(Rails.root, "out.data")
+      @params = Marshal.load(File.read(File.join(Rails.root, "out.data")) )
+    else
+      @params = {}
+    end
+  end
+
+  def update
     @params = params.permit(:temp, :hum, :current_year, :current_month, :current_day, :current_time)
+    File.open(File.join( Rails.root, "out.data"), "w") { |f| f.write(Marshal.dump(@params)) }
+    redirect_to :root
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :data
+
   root 'public#index'
+  get '/update', to: 'public#update'
 end
