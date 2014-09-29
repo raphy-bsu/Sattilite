9bfea79e821968142695e3675ed804dbe8292063
Ilya Berinchik <ilya@Samsung-RV-509.(none)>
2014-09-29 23:25:19 +0300

    #1 fixed Add strings file host

--- script/seed.rb
+++ script/seed.rb
@@ -1,6 +1,13 @@
 require 'rest_client'
 require 'csv'
 
-  CSV.foreach('data/seed_data.csv', headers: true)  do |row|
-     RestClient.get 'http://localhost:3000/update', :params => {:temp => row['temp'], :hum => row['hum']} 
-  end  
+	host = "localhost:3000"
+	file = 'data/seed_data.csv'
+
+	if File.exists?(file)
+		CSV.foreach(file, headers: true)  do |row|
+	      RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']}) 
+	    end 
+	else
+		puts "File not found"
+	end
