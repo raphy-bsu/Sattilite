87f0993f6107679e92b91faa66f9a3e836cf398e
Ilya Berinchik <ilya@Samsung-RV-509.(none)>
2014-09-30 15:59:12 +0300

    fixes#1 Almostly

--- script/seed.rb
+++ script/seed.rb
@@ -5,9 +5,9 @@ host = "localhost:3000"
 file = 'data/seed_data.csv'
 
 if File.exists?(file)
-	CSV.foreach(file, headers: true)  do |row|
+  CSV.foreach(file, headers: true)  do |row|
     RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']})
   end
 else
-	puts "File not found"
+  puts "File not found"
 end
