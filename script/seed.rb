6274587bf7d821eeadbc6abf9761850019a6384a
Ilya Berinchik <ilya@Samsung-RV-509.(none)>
2014-09-30 15:52:06 +0300

    fixes#1 Spacing

--- script/seed.rb
+++ script/seed.rb
@@ -6,8 +6,8 @@ file = 'data/seed_data.csv'
 
 if File.exists?(file)
 	CSV.foreach(file, headers: true)  do |row|
-      RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']})
-    end
+    RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']})
+  end
 else
 	puts "File not found"
 end
