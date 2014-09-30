c3e77556f85410b1666efaba7ff7ac4cf037856c
Ilya Berinchik <ilyabyar@gmail.com>
2014-10-01 02:28:28 +0300

    #4 - Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'public/about'
 
   resources :infos
 
   root 'infos#index'
   get '/update', to: 'infos#update'
   post '/clean', to: 'infos#clean', as: :clean_infos
+  get '/about', to: 'public#about'
 end
