bc8aeda7e5b2b14d1c23c4843492a0eb807ea477
Ilya Berinchik <ilya@Samsung-RV-509.(none)>
2014-09-30 15:18:12 +0300

    fixes#1 Delete spaces

--- script/seed.rb
+++ script/seed.rb
@@ -1,13 +1,13 @@
 require 'rest_client'
 require 'csv'
 
-	host = "localhost:3000"
-	file = 'data/seed_data.csv'
+host = "localhost:3000"
+file = 'data/seed_data.csv'
 
-	if File.exists?(file)
-		CSV.foreach(file, headers: true)  do |row|
-	      RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']}) 
-	    end 
-	else
-		puts "File not found"
-	end
+if File.exists?(file)
+	CSV.foreach(file, headers: true)  do |row|
+      RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']})
+    end
+else
+	puts "File not found"
+end
