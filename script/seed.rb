708eb2598b2ab809e30cae86b0b34e3c27555f8a
Ilya Berinchik <ilyabyar@gmail.com>
2014-09-30 18:49:02 +0300

    #1 - Add exception in  block

--- script/seed.rb
+++ script/seed.rb
@@ -7,18 +7,21 @@ puts "Script is running...".colorize(:blue)
 host = ARGV.first || "localhost:3000"
 file = 'data/seed_data.csv'
 
-begin
+
   if File.exists?(file)
     puts "SCV file exists".colorize(:green)
     CSV.foreach(file, headers: true)  do |row|
-      RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']})
+      begin
+        RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']})
+      rescue Errno::ECONNREFUSED => msg
+        puts (msg.to_s + ". Probably, try to start your local server").colorize(:red)
+        break
+      end
       puts "Successfully sent data!".colorize(:green)
     end
   else
     puts "File not found".colorize(:red)
   end
-rescue SystemCallError => msg
-  puts (msg.to_s + ". Probably, try to start your local server").colorize(:red)
-end
+
 
 
