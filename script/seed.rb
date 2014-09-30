e93cabf16b0071d5391d4419b61b4ea17e24a044
Ilya Berinchik <ilyabyar@gmail.com>
2014-09-30 19:11:15 +0300

    #1- Refactor call to .color and  puts method send out from loop

--- script/seed.rb
+++ script/seed.rb
@@ -2,25 +2,25 @@ require 'rest_client'
 require 'csv'
 require 'colorize'
 
-puts "Script is running...".colorize(:blue)
+puts "Script is running...".blue
 
 host = ARGV.first || "localhost:3000"
 file = 'data/seed_data.csv'
 
 
   if File.exists?(file)
-    puts "SCV file exists".colorize(:green)
+    puts "SCV file exists".green
     CSV.foreach(file, headers: true)  do |row|
       begin
         RestClient.get(host + '/update', :params => {:temp => row['temp'], :hum => row['hum']})
       rescue Errno::ECONNREFUSED => msg
-        puts (msg.to_s + ". No connection to the server.").colorize(:red)
+        puts (msg.to_s + ". No connection to the server.").red
         break
       end
-      puts "Successfully sent data!".colorize(:green)
     end
+    puts "Successfully sent data!".green
   else
-    puts "File not found".colorize(:red)
+    puts "File not found".red
   end
 
 
