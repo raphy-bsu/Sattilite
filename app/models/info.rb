41ea0790893d13ecd7a3455712bf7d7f9de81d9d
Orel Sokolov <orelcokolov@gmail.com>
2015-09-10 16:29:22 +0300

    Annotate models

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: infos
+#
+#  id         :integer          not null, primary key
+#  temp       :string(255)
+#  hum        :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Info < ActiveRecord::Base
 end
