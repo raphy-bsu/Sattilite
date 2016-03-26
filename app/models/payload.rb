144a85fefe6a658d16f2bc8bb60eeb53afe0c58e
Orel Sokolov <orelcokolov@gmail.com>
2016-03-26 16:24:43 +0300

    Annotate

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: payloads
+#
+#  id          :integer          not null, primary key
+#  time        :integer
+#  temperature :float
+#  humidity    :float
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Payload < ActiveRecord::Base
 end
--- test/models/payload_test.rb
+++ test/models/payload_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: payloads
+#
+#  id          :integer          not null, primary key
+#  time        :integer
+#  temperature :float
+#  humidity    :float
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'test_helper'
 
 class PayloadTest < ActiveSupport::TestCase
