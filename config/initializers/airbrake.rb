774726602e213472d761958f51896968b9bf5f73
Oleg Orlov <orelcokolov@gmail.com>
2016-03-28 17:17:17 +0300

    Update key

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
   config.host = 'http://errbit-orelsokolov.herokuapp.com'
   config.project_id = true
-  config.project_key = '11410d43e72716b39a2c315f039f52df'
+  config.project_key = 'b4b930176907528ca5a510d649b673df'
 end
