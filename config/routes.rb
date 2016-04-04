e5a68359148cd9336cd9f168b5309a30c04dfe59
Oleg Orlov <orelcokolov@gmail.com>
2016-04-04 14:35:55 +0300

    Back get

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :infos
 
   root 'infos#index'
-  post '/update', to: 'infos#update'
+  get '/update', to: 'infos#update'
   post '/clean', to: 'infos#clean', as: :clean_infos
   get '/about', to: 'public#about'
 end
