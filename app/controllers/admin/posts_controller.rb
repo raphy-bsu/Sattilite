9d50d458862b9e0e62d656fa6079f681bbbb1fcd
Oleg Orlov <orelcokolov@gmail.com>
2016-05-22 16:09:54 +0300

    Update posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PostsController < Admin::AdminController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page])
   end
 
   def show
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.paginate(:page => params[:page], per_page: 5)
   end
 
   def show
