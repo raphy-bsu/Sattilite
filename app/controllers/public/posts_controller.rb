0f657add6286ee677aac236da733b79811147e08
Oleg Orlov <orelcokolov@gmail.com>
2016-05-23 10:10:45 +0300

    Update posts

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,9 +1,9 @@
-class PostsController < ApplicationController
+class Public::PostsController < ApplicationController
   def index
     @posts = Post.paginate(:page => params[:page], per_page: 5)
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 end
