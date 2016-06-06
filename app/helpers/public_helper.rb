feb3d4acb4d2023cd25a2967518f24c3fb83187d
Oleg Orlov <orelcokolov@gmail.com>
2016-06-06 15:36:22 +0300

    Update main page

--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,7 +1,7 @@
 module PublicHelper
-  def fancyimage(name)
+  def fancyimage(name, width = 220)
     content_tag :a, href: image_url(name), class: 'fancy' do
-      content_tag :img, nil, { src: image_url(name), width: 220 }
+      content_tag :img, nil, { src: image_url(name), width: width }
     end
   end
 end
